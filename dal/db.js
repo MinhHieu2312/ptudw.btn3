6bc78c8bf485eeeec35dce300f46b52693955695
TrongHoang21 <73478391+TrongHoang21@users.noreply.github.com>
2021-01-15 06:49:28 +0700

    Add files via upload
    Sua lai database connect success

--- dal/db.js
+++ dal/db.js
@@ -9,10 +9,54 @@ async function connect() {
             useFindAndModify: false,
             useCreateIndex: true,
         });
-        console.log('Connect successful!');
+        console.log('Database connect successful!');
     } catch (error) {
-        console.log('Connect fail!');
+        console.log('Database connect fail!');
     }
 }
 
 module.exports = { connect };
+
+
+// const { MongoClient } = require('mongodb');
+
+// const uri = 'mongodb+srv://sbookshop:ptudw123@cluster0.em6hz.mongodb.net/test';
+
+// const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+// let database;
+
+// async function connectDb(){
+//     try{
+//     await client.connect();
+    
+//     database = await client.db('sbookshop');
+//     console.log('Db connected!');
+//     }
+//     catch(err)
+//     {
+//         console.log(err);
+//     }
+// }
+
+// console.log('RUNNING DB...');
+
+// connectDb();
+
+// //Set a name for the db()
+// const db = () => database;
+
+// module.exports.db = db;
+
+
+// const insertDocuments = function(db, user, callback) {
+//     // Get the documents collection
+//     const collection = db.collection('users');
+    
+
+//     //Insert ONE document
+//     collection.insertOne({user.name: "noel vui ve"}, function(err, result) {
+//         console.log("Inserted ONE document into the collection");
+//         callback(result);
+//         });
+//   }
